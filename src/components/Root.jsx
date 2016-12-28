579dea35e8e99515f19f381cebb6a8bd8e3247a9
Daniel <oopdaniel@gmail.com>
2016-12-28 23:20:40 +0800

    refactor the entry component

---
+++ src/components/Root.jsx
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react';
+import { Provider } from 'react-redux';
+import { Router, IndexRoute, Route } from 'react-router';
+
+import App from './App/App';
+import Dashboard from './Dashboard/Dashboard';
+import Trend from './Trend/Trend';
+import Detail from './Detail/Detail';
+import './styles.css';
+
+const propTypes = {
+  store: PropTypes.object.isRequired,
+  history: PropTypes.object,
+};
+
+const Root = ({ store, history }) => (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Dashboard} />
+        <Route path="/trend" component={Trend} />
+      </Route>
+      <Route path="/detail" component={Detail} />
+    </Router>
+  </Provider>
+);
+
+Root.propTypes = propTypes;
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { syncHistoryWithStore } from 'react-router-redux';
-import { Router, IndexRoute, Route, browserHistory } from 'react-router';
+import { browserHistory } from 'react-router';
 
 import configureStore from './store/configureStore';
-import App from './components/App/App';
-import Dashboard from './components/Dashboard/Dashboard';
-import Trend from './components/Trend/Trend';
-import Detail from './components/Detail/Detail';
-import './styles.css';
+import Root from './components/Root';
 
 
 const store = configureStore();
@@ -18,16 +13,10 @@ const history = syncHistoryWithStore(browserHistory, store);
 // Required for replaying actions from devtools to work
 // reduxRouterMiddleware.listenForReplays(store);
 
-
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Dashboard} />
-        <Route path="/trend" component={Trend} />
-      </Route>
-      <Route path="/detail" component={Detail} />
-    </Router>
-  </Provider>,
+  <Root
+    store={store}
+    history={history}
+  />,
   document.getElementById('app'),
 );
