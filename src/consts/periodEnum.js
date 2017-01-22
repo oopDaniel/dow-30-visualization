7e671a496a99c3a651090950f85cc46bcd80ef1f
Daniel <oopdaniel@gmail.com>
2017-01-22 20:53:21 +0800

    add two getter for period

--- src/consts/periodEnum.js
+++ src/consts/periodEnum.js
@@ -6,19 +6,27 @@ export const threeMonth = '3 months';
 export const sixMonth   = '6 months';
 export const oneYear    = '1 year';
 
-const periodEnum = {
+export const GetNameByPeriod = {
   0: today,
   1: oneWeek,
   2: oneMonth,
   3: threeMonth,
   4: sixMonth,
   5: oneYear,
+};
+
+export const GetPeriodByName = {
   [today]: 0,
   [oneWeek]: 1,
   [oneMonth]: 2,
   [threeMonth]: 3,
   [sixMonth]: 4,
   [oneYear]: 5,
+};
+
+const periodEnum = {
+  ...GetPeriodByName,
+  ...GetNameByPeriod,
 };
 
 export default periodEnum;
